28d4b3e68ccce36add788f0ba53d97259057e896
Stephen Elson <stephen.elson@gmail.com>
2024-04-10 13:58:15 +0200

    feat!: add TS types, remove ES module dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
-import clear from 'rollup-plugin-clear';
+import del from 'rollup-plugin-delete';
 import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+      },
+    ],
+    external: ['dom-parser'],
+    plugins: [typescript(), del({ targets: ['dist'] })],
+  },
+  {
+    input: 'dist/build/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
     },
-    {
-      file: 'dist/index.mjs',
-      format: 'esm',
-    },
-  ],
-  external: ['dom-parser'],
-  plugins: [typescript(), clear({ targets: ['dist'] })],
-};
+    plugins: [dts(), del({ targets: ['dist/build'], hook: 'buildEnd' })],
+  },
+];
